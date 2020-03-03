494f0f2d3a89becea98a853be129952bb5880284
Tadzio <tadzio.holtrop@vu.nl>
2020-03-02 17:30:10 -0800

    update

---
+++ .ipynb_checkpoints/setup-checkpoint.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='hydropt',
+      version='0.1',
+      description='HYDROPT',
+      url='',
+      author='',
+      author_email='',
+      license=',
+      packages=['hydropt'],
+      install_requires=[
+          'numpy',
+      ],
+      zip_safe=False)
