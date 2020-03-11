aa45d0d422038f8010c4f6327ac9536daaa9b6af
Tadzio <tadzio.holtrop@vu.nl>
2020-03-10 17:38:55 -0700

    updated setup requirements

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='hydropt',
-      version='0.1',
+      version='0.1.0',
       description='HYDROPT',
       url='',
       author='',
@@ -10,5 +10,7 @@ setup(name='hydropt',
       packages=['hydropt'],
       install_requires=[
           'numpy',
-      ],
+          'pandas',
+          'matplotlib',
+          'sklearn'],
       zip_safe=False)
