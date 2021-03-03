57261205b3dbf928db217dda9cdad9e9ee50f441
Tadzio <tadzio.holtrop@vu.nl>
2021-03-03 10:41:07 +0000

    update

--- hydropt/tests/test.py
+++ hydropt/tests/test.py
@@ -1,15 +1,21 @@
 from ..bio_opts import TwoCompModel
 from ..hydropt import PolynomialForward
 from jax import jacfwd
+
+'''
+Testing jacfwd from jax lib
+
+- import jax.numpy as np in hydropt module
+- change return type from ForwardModel().forward() - should return numpy.array, not pd.Series
+'''
 #
 bom = TwoCompModel()
 fwd_model = PolynomialForward(bom)
 fwd_l = lambda w: fwd_model.forward(**w)
 
-import jax.numpy as np
 p = jacfwd(fwd_l)({
     'nap': 22.0,
     'cdom': 22.0})  
 
-q = fwd_model.jacobian(nap = 22.0, cdom=22.0)
+q = fwd_model.jacobian(nap=22.0, cdom=22.0)
 pass
